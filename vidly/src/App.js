6f42d0d59f7f60844366e105071a070f498aa55b
dmarinov8 <demarinov@gmail.com>
2019-11-05 13:30:34 +0200

    initial commit

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navbar";
 import "./App.css";
 
 class App extends Component {
@@ -7,9 +12,19 @@ class App extends Component {
 
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
