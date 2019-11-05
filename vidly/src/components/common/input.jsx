d7a932f557d74569d70118e5f4b5b413aaa161ba
dmarinov8 <demarinov@gmail.com>
2019-11-05 16:53:36 +0200

    initial commit

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-const Input = ({ name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
         autoFocus
-        value={value}
-        onChange={onChange}
-        type="text"
+        name={name}
         className="form-control"
         id={name}
-        name={name}
+        {...rest}
       />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
